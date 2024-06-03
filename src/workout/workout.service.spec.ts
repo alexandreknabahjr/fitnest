83bed2b7a3a5e8d66e524a2bd0241ab2f39221fb
Alexandre Knabah <knabah.alexandre@gmail.com>
2024-06-03 13:50:17 -0300

    workout service tests look all right

--- src/workout/workout.service.spec.ts
+++ src/workout/workout.service.spec.ts
@@ -2,55 +2,66 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { WorkoutService } from './workout.service';
 import { PrismaService } from '../prisma/prisma.service';
 
+const fakeWorkout = [{
+  name: "Crossfit Session I",
+  duration: 60,
+  date: "2023-06-03T09:00:00Z",
+  workoutType: "CROSSFIT",
+  customerId: "22911902-a8c5-4ead-b562-b0f8865e95c3"
+}]
+
+const prismaMock = {
+  workout: {
+    create: jest.fn().mockReturnValue(fakeWorkout[0]),
+    findMany: jest.fn().mockReturnValue(fakeWorkout),
+    delete: jest.fn()
+  }
+}
+
 describe('WorkoutService', () => {
   let service: WorkoutService;
+  let prisma: PrismaService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WorkoutService],
-    }).useMocker((token) => {
-      if(token === PrismaService){
-        return {
-          workout: {
-            create: jest.fn().mockResolvedValue({
-              name: "Crossfit Session I",
-              duration: 60,
-              date: "2023-06-03T09:00:00Z",
-              workoutType: "CROSSFIT",
-              customerId: "22911902-a8c5-4ead-b562-b0f8865e95c3"
-            })
-          }
+      providers: [WorkoutService,
+        {
+          provide: PrismaService,
+          useValue: prismaMock
         }
-      }
+      ]
     }).compile();
 
     service = module.get<WorkoutService>(WorkoutService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
   describe('createWorkout', () => {
-    it('should create a workout', async () => {
-      const newWorkout = {
-          name: "Crossfit Session I",
-          duration: 60,
-          date: "2023-06-03T09:00:00Z",
-          workoutType: "CROSSFIT",
-          customerId: "22911902-a8c5-4ead-b562-b0f8865e95c3"
-      }
 
-      const workoutResult = await service.createWorkout(newWorkout);
+    it('should create a new workout', async () => {
 
-      expect(workoutResult).toEqual({
-        name: "Crossfit Session I",
-        duration: 60,
-        date: "2023-06-03T09:00:00Z",
-        workoutType: "CROSSFIT",
-        customerId: "22911902-a8c5-4ead-b562-b0f8865e95c3"
-      });
+      const response = await service.createWorkout(fakeWorkout[0]);
 
+      expect(response).toBe(fakeWorkout[0]);
+      expect(prisma.workout.create).toHaveBeenCalledTimes(1);
+    })
+  });
+
+  describe('findAllWorkouts', () => {
+    it('should return all workouts', async () => {
+
+      const response = await service.findAllWorkouts();
+
+      expect(response).toBe(fakeWorkout);
+      expect(prisma.workout.findMany).toHaveBeenCalledTimes(1);
     })
   })
 
